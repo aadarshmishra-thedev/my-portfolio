ed2a34959992a32c0d15eaa5bdae9d458f1bc964
AadarshMishra <122502231+aadarshmishra-thedev@users.noreply.github.com>
2026-02-26 07:15:40 +0000

    Update profile information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aadarsh",
+  title: "AI Developer & Automation Engineer",
+  email: "aadarshmishra2024@gmail.com",
+  gitHub: "aadarshmishra-thedev",
+  instagram: "hustle_with_aadarsh",
+  linkedIn: "Aadarsh Mishra",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
